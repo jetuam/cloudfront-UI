69f1a81179d18e9e4e94ec3e34262756272a6ec5
Jayateerth Mirji <48415313+jetuam@users.noreply.github.com>
2021-08-09 10:51:46 +0530

    Update deploy.sh

--- deploy_script/deploy.sh
+++ deploy_script/deploy.sh
@@ -1,1 +1,9 @@
+cloudfront_distribution_id=$(aws ssm get-parameter \
+   --name "/kicl/$REGION_NAME/usermgmtui/$CI_ENVIRONMENT_NAME/CdnDistributionId" \
+   --query "parameter.value" \
+   --output text)
+   
+echo "Cloudfront distribution ID : "$cloudfront_distribution_id
 
+#invalidate cloudfront cache
+aws cloudfront create-invalidation --distribution-id ${cloudfront_distribution_id} --path "/*"
